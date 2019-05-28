0556c769ef116e9a54c7ad3237cd9a30f467bba8
Max <max26@ksu.edu>
2019-05-28 13:41:22 -0500

    Add project files.

---
+++ Ksu.Cis300.TextEditor/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Ksu.Cis300.TextEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxEditorMenu = new System.Windows.Forms.MenuStrip();
+            this.uxFileButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxOpenButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxSaveAsButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxTextBox1 = new System.Windows.Forms.TextBox();
+            this.uxOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.uxSaveFile = new System.Windows.Forms.SaveFileDialog();
+            this.uxEditorMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uxEditorMenu
+            // 
+            this.uxEditorMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uxFileButton});
+            this.uxEditorMenu.Location = new System.Drawing.Point(0, 0);
+            this.uxEditorMenu.Name = "uxEditorMenu";
+            this.uxEditorMenu.Size = new System.Drawing.Size(624, 24);
+            this.uxEditorMenu.TabIndex = 0;
+            this.uxEditorMenu.Text = "menuStrip1";
+            // 
+            // uxFileButton
+            // 
+            this.uxFileButton.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uxOpenButton,
+            this.uxSaveAsButton});
+            this.uxFileButton.Name = "uxFileButton";
+            this.uxFileButton.Size = new System.Drawing.Size(37, 20);
+            this.uxFileButton.Text = "File";
+            // 
+            // uxOpenButton
+            // 
+            this.uxOpenButton.Name = "uxOpenButton";
+            this.uxOpenButton.Size = new System.Drawing.Size(180, 22);
+            this.uxOpenButton.Text = "Open";
+            this.uxOpenButton.Click += new System.EventHandler(this.UxOpenButton_Click);
+            // 
+            // uxSaveAsButton
+            // 
+            this.uxSaveAsButton.Name = "uxSaveAsButton";
+            this.uxSaveAsButton.Size = new System.Drawing.Size(180, 22);
+            this.uxSaveAsButton.Text = "Save As";
+            this.uxSaveAsButton.Click += new System.EventHandler(this.UxSaveAsButton_Click);
+            // 
+            // uxTextBox1
+            // 
+            this.uxTextBox1.Location = new System.Drawing.Point(12, 55);
+            this.uxTextBox1.Multiline = true;
+            this.uxTextBox1.Name = "uxTextBox1";
+            this.uxTextBox1.Size = new System.Drawing.Size(589, 416);
+            this.uxTextBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 517);
+            this.Controls.Add(this.uxTextBox1);
+            this.Controls.Add(this.uxEditorMenu);
+            this.MainMenuStrip = this.uxEditorMenu;
+            this.Name = "Form1";
+            this.Text = "Text Editor";
+            this.uxEditorMenu.ResumeLayout(false);
+            this.uxEditorMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip uxEditorMenu;
+        private System.Windows.Forms.ToolStripMenuItem uxFileButton;
+        private System.Windows.Forms.ToolStripMenuItem uxOpenButton;
+        private System.Windows.Forms.ToolStripMenuItem uxSaveAsButton;
+        private System.Windows.Forms.TextBox uxTextBox1;
+        private System.Windows.Forms.OpenFileDialog uxOpenFile;
+        private System.Windows.Forms.SaveFileDialog uxSaveFile;
+    }
+}
+
---
+++ Ksu.Cis300.TextEditor/Form1.cs
@@ -0,0 +1,54 @@
+﻿/* Form1.cs
+ * Author: Max Maus 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ksu.Cis300.TextEditor
+{
+    /// <summary>
+    /// Constructs a GUI for a program that allows one to open, edit, and save text files.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Constructs the GUI
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// Open a file of user's choice
+        private void UxOpenButton_Click(object sender, EventArgs e)
+        {
+            if (uxOpenFile.ShowDialog() == DialogResult.OK)
+            {
+                string fn = uxOpenFile.FileName; // // Process the file. //
+                MessageBox.Show("Cannot open file" + fn);
+
+            }
+        }
+
+        /// <summary>
+        /// Saves a file of user's choice
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UxSaveAsButton_Click(object sender, EventArgs e)
+        {
+            if (uxSaveFile.ShowDialog() == DialogResult.OK)
+            {
+                string fn = uxSaveFile.FileName; // // Process the file. // 
+                MessageBox.Show("Cannot save file" + fn);
+            }
+        }
+    }
+    }
